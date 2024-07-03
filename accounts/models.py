1c739a5a3439e723332ff6d4dffb43041a9b2d4f
izhar <ihansari2016@gmail.com>
2024-07-03 21:39:53 +0530

    removed unused commented liines

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,6 @@
 from django.db import models
-
-# Create your models here.
-# from datetime import datetime
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from ecommapp.settings import JWT_EXPIRY_TIME
-# from accounts.utils import get_jwt_payload
 
 
     
@@ -19,13 +15,9 @@ class MyUser(AbstractUser):
     is_staff=models.BooleanField(default=True)
     is_admin=models.BooleanField(null= True, default=False)
     phone_no=models.CharField(max_length=10, blank=True)
-    # token_expiry_time = models.IntegerField(null=True, blank=True, default=JWT_EXPIRY_TIME)
     groups = models.ManyToManyField(Group,related_name='myuser_set',blank=True,help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',related_query_name='user')
     user_permissions = models.ManyToManyField(Permission,related_name='myuser_set',blank=True,help_text='Specific permissions for this user.',related_query_name='user')
 
-    # class Meta:
-    #     verbose_name = 'user'
-    #     verbose_name_plural = 'users'
 
     def __str__(self):
         return self.username
